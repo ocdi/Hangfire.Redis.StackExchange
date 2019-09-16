f88aa401358f8d38134fb95fff4b4986ecc96edd
Trevor Nichols <trevor@theredfoxgroup.com.au>
2019-09-16 20:30:07 +1000

    Add sample apps to test this out

---
+++ AspNetCoreSample/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using NetCoreSample;
+using System;
+
+namespace AspNetCoreHost
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+         
+            services.AddHangfire((provider, configuration) => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRedisStorage());
+
+            services.AddHostedService<RecurringJobsService>();
+
+            services.AddHangfireServer(options =>
+            {
+                options.StopTimeout = TimeSpan.FromSeconds(15);
+                options.ShutdownTimeout = TimeSpan.FromSeconds(30);
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHangfireDashboard();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
